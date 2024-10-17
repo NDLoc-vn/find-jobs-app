0118807eec73b15c4e3190aa95e5ae52f002f16c
NDLoc-vn <loccoc1807@gmail.com>
2024-10-17 12:14:31 +0700

    make recuiter-manager a empty module

---
+++ app/recruiter-manager/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import Header from "../ui/company/Header";
+
+const RecruiterManager = () => {
+  return (
+    <div>
+      <Header />
+      <p className="pt-3">Coming soon!</p>
+    </div>
+  );
+};
+
+export default RecruiterManager;
