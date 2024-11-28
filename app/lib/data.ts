0c1f16d5ab4db6e9b71a738721443d44011d2657
NDLoc-vn <loccoc1807@gmail.com>
2024-11-28 08:48:55 +0700

    pagination and add categories data

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,49 +1,65 @@
+export const categories = [
+  { id: "1", name: "Graphics & Design" },
+  { id: "2", name: "Code & Programming" },
+  { id: "3", name: "Digital Marketing" },
+  { id: "4", name: "Video & Animation" },
+  { id: "5", name: "Writing & Translation" },
+  { id: "6", name: "Music & Audio" },
+  { id: "7", name: "Business & Finance" },
+  { id: "8", name: "Lifestyle & Wellness" },
+  { id: "9", name: "Photography" },
+  { id: "10", name: "IT & Networking" },
+  { id: "11", name: "Engineering" },
+  { id: "12", name: "Education & Training" },
+  { id: "13", name: "Consulting" },
+];
+
 export async function fetchJobs() {
   // delay de demo
-  await new Promise(resolve => setTimeout(resolve, 1000));
+  await new Promise((resolve) => setTimeout(resolve, 1000));
 
   return [
     {
       title: "Junior Graphic Designer",
       company: "Google Inc.",
       salary: "$20,000 - $25,000",
-      location: "Da Nang, Viet Nam"
+      location: "Da Nang, Viet Nam",
     },
     {
       title: "Junior Graphic Designer",
       company: "Google Inc.",
       salary: "$20,000 - $25,000",
-      location: "Da Nang, Viet Nam"
+      location: "Da Nang, Viet Nam",
     },
     {
       title: "Junior Graphic Designer",
       company: "Google Inc.",
       salary: "$20,000 - $25,000",
-      location: "Da Nang, Viet Nam"
+      location: "Da Nang, Viet Nam",
     },
     {
       title: "Junior Graphic Designer",
       company: "Google Inc.",
       salary: "$20,000 - $25,000",
-      location: "Da Nang, Viet Nam"
+      location: "Da Nang, Viet Nam",
     },
     {
       title: "Junior Graphic Designer",
       company: "Google Inc.",
       salary: "$20,000 - $25,000",
-      location: "Da Nang, Viet Nam"
+      location: "Da Nang, Viet Nam",
     },
     {
       title: "Junior Graphic Designer",
       company: "Google Inc.",
       salary: "$20,000 - $25,000",
-      location: "Da Nang, Viet Nam"
+      location: "Da Nang, Viet Nam",
     },
   ];
 }
 
 export async function fetchCompanyAccount() {
-  await new Promise(resolve => setTimeout(resolve, 500));
+  await new Promise((resolve) => setTimeout(resolve, 500));
   return [
     {
       name: "Google Inc.",
@@ -66,8 +82,8 @@ export async function fetchCompanyAccount() {
         address: "456 Nguyễn Lương Bằng",
       },
       website: "https://facebook.com",
-    }
-  ]
+    },
+  ];
 }
 
 export const predefinedSkills = [
@@ -178,5 +194,4 @@ export const predefinedSkills = [
   "2Vercel",
   "2CI/CD",
   "2Agile",
-
-]
+];
